f26de5b518f203613bcff47b0a0d42de6f5c06ed
Ptotem Learning <info@ptotem.com>
2012-08-31 16:35:40 +0530

    Finalization Version 1

--- db/migrate/20120315042250_create_contact_types_contacts_join_table.rb
+++ db/migrate/20120315042250_create_contact_types_contacts_join_table.rb
@@ -2,7 +2,7 @@ class CreateContactTypesContactsJoinTable < ActiveRecord::Migration
   def change
     create_table :contact_types_contacts, :id => false do |t|
       t.integer :contact_id
-      t.integer :contact_type_id
+      t.integer :contact_type_id, :default=>1
     end
   end
 end
