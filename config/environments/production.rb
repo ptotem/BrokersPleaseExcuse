7a3997e1cd95af4c5ddd1a129a936d07b2be9f9b
Ptotem Learning <info@ptotem.com>
2012-09-06 12:19:25 +0530

    Post UAT Corrections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,11 @@ BPEv075::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
+
+  config.whiny_nils = true
+
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
