535e6ae0cf7695016f191628c5c2e4fca9c60ad7
Ptotem Learning <info@ptotem.com>
2012-09-27 23:36:28 +0530

    Surveyor Show

--- app/models/poi_type.rb
+++ app/models/poi_type.rb
@@ -2,9 +2,6 @@ class PoiType < ActiveRecord::Base
   has_many :pois
   has_many :localities, :through => :pois
 
-  has_attached_file :avatar,
-                    :path => ":rails_root/app/assets/images/article_images/:id/:style_:basename.:extension",
-                    :url => "/assets/article_images/:id/:style_:basename.:extension"
-
+  has_attached_file :avatar
 
 end
